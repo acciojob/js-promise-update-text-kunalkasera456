7200da83b67966d0e34c26e9c0af44ac55f2aa16
kunal kumar <105341082+kunalkasera456@users.noreply.github.com>
2023-06-30 11:30:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function generatePromise {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve("Hello, world!");
+		}, 1000)
+	})
+}
+
+generatePromise.then((data) => {
+	const text = document.ElementById("output");
+	text.innerText = data;
+})
